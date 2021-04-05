386969789b304b91ae27e008ea2ddb9ae47267c8
KevinBruce2018 <20171101074@stumail.hbu.edu.cn>
2021-04-05 15:57:27 +0800

    修复了logo在某些情况下不显示的bug

--- Client/client.py
+++ Client/client.py
@@ -67,7 +67,10 @@ class UI_MainWindow(QWidget):
         self.setLayout(layout)
 
         self.pic = QLabel()
-        self.pic.setPixmap(QPixmap('logo.png'))
+        #使用绝对路径 解决不在当前路径运行时logo不显示的问题
+        logo_path = os.path.abspath(__file__).split('/')[:-1]
+        logo_path = '/'.join(logo_path)+'/logo.png'
+        self.pic.setPixmap(QPixmap(logo_path))
         self.pic.resize(90,90)
         #self.pic.setScaledContents(True)
 
