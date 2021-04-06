ff692a6387dd537d15ab79d943e6f31cc2ce54a5
KevinBruce2018 <20171101074@stumail.hbu.edu.cn>
2021-04-06 21:55:40 +0800

    封装了一个登录组件

--- Client/CustomWidget.py
+++ Client/CustomWidget.py
@@ -14,3 +14,34 @@ class ClickLabel(QLabel):
     # 可在外部与槽函数连接
     def connect_customized_slot(self, func):
         self.button_clicked_signal.connect(func)
+
+class LoginButton(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.login_btn = QPushButton('登      录',self)
+        self.forget_pass = ClickLabel(self)
+        self.forget_pass.setText('忘记密码')
+        self.reg_label = ClickLabel(self)
+        self.reg_label.setText('立即注册')
+        self.draw()
+    def draw(self):
+        self.login_btn.setFixedHeight(40)
+        self.login_btn.setFixedWidth(316)
+        self.login_btn.setFont(QFont('Arial',18))
+        self.login_btn.setStyleSheet('background-color:#49a8f8;color:rgb(255,255,255)')
+        self.forget_pass.move(0,44)
+        self.reg_label.move(260,44)
+        self.forget_pass.setStyleSheet(
+            'QLabel{color:blue;}'
+            'QLabel:hover{color:white;}'
+        )
+        self.reg_label.setStyleSheet(
+            'QLabel{color:blue;}'
+            'QLabel:hover{color:white;}'
+        )
+    def setRegisterFunc(self,func):
+        self.reg_label.connect_customized_slot(func)
+    def setForgetPassFunc(self,func):
+        self.forget_pass.connect_customized_slot(func)
+    def setLoginFunc(self,func):
+        self.login_btn.clicked.connect(func)
